1245967866512b1192f28941abe414f7436a19f1
Hardison <hardisoncyrus@gmail.com>
2024-12-19 17:34:10 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
-        maven("https://mirrors.cloud.tencent.com/nexus/repository/google")
-        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
         google() // Fallback to Google repository
         mavenCentral() // Fallback to Maven Central repository
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/google")
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.1")
@@ -29,12 +29,13 @@ plugins {
 
 allprojects {
     repositories {
-        maven("https://mirrors.cloud.tencent.com/nexus/repository/google")
-        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
-        maven("https://jitpack.io")
         // add google mavenrepo & maven central
         google()
         mavenCentral()
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/google")
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
+        maven("https://jitpack.io")
+        maven("https://maven.google.com")
     }
     tasks.withType<KotlinCompile> {
         kotlinOptions {
@@ -58,21 +59,9 @@ allprojects {
     apply(plugin = "jacoco")
 }
 
-// Include QRGen dependency using JitPack
-subprojects {
-    dependencies {
-        implementation("com.github.kenglxn:QRGen:3.0.1") // Resolving QRGen from JitPack
-    }
-}
-
 // Setup all reports aggregation
 apply(from = "jacoco_aggregation.gradle.kts")
 
 tasks.register<Delete>("clean").configure {
     delete(rootProject.buildDir)
-}
-
-// Exclude native libraries if not needed
-configurations.all {
-    exclude(group = "com.github.kenglxn.QRGen", module = "android")
 }
